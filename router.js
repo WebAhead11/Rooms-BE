1474545634f18950a7576f4dc594459d1bcd48cb
kassim <kassimbashir@gmail.com>
2021-10-26 11:38:40 +0300

    set up usename Relates #2

--- router.js
+++ router.js
@@ -18,5 +18,16 @@ router.get("/rooms", (req, res,next) => {
 router.post("/create-room",(req,res)=>{
 
 })
+router.post("/create-user",(req,res,next)=>{
+  let username = req.body.username;
+  console.log(username);
+  db.query("INSERT INTO users(username) VALUES($1)", [req.body.username])
+  .then(result =>{
+    db.query("SELECT * from users")
+    .then(res => console.log(res.rows));
+  })
+  .catch(next);
+
+})
 
 export default router;
